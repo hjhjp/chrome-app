bedccd392147b0441f0b27735316e00279caf470
hjhjp <hjhjp0206@gmail.com>
2022-02-21 17:08:26 +0900

    #5.2 PadStart

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,13 @@ const clock = document.querySelector("h2#clock");
 
 function getClock () {
   const date = new Date();
-  clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+  //padStart(글자길이, 앞에 들어가야하는 글자)는 string이 가져야하는 글자 길이를 설정
+  //pasEnd(글자일이, 뒤에 들어가야하는 글자)
+  //string()을 이용해 숫자를 텍스트로 변환
+  const hours = String(date.getHours()).padStart(2,"0");
+  const minutes = String(date.getMinutes()).padStart(2,"0");
+  const seconds = String(date.getSeconds()).padStart(2,"0");
+  clock.innerText = (`${hours}:${minutes}:${seconds}`);
 }
 
 getClock ();
